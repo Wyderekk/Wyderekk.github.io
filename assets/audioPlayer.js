9237a94960a050a44f5b7c576a8b5bb3519367f8
wyderekk <wyderrr1337@gmail.com>
2023-05-04 00:40:56 +0200

    idk

---
+++ assets/audioPlayer.js
@@ -0,0 +1,11 @@
+function audioPlay() {
+    var audio = document.getElementById("audio");
+    audio.volume = 0.4;
+    audio.play()
+}
+
+function videoPlay() {
+    var video = document.getElementById("video");
+    video.volume = 0;
+    video.play()
+}
---
+++ assets/text.js
@@ -0,0 +1,6 @@
+new Typed('#typed', {
+    strings: ['wyderekk', 'java developer'],
+    typeSpeed: 50,
+    delaySpeed: 90,
+    loop: true
+});
